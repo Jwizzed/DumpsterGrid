ce73c6ae39ecc8e632384596efc18155a83258d9
Krittin SETDHAVANICH <krittin.set@ku.th>
2026-05-29 16:19:46 +0700

    feat: add privacy policy and terms of service pages with sitemaps

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://dumpsterflow.com',
+  site: 'https://dumpstergrid.com',
   integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()]
